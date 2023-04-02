a0908c25a3c2d72fb742aeeb862b90b7ced7ef08
neem693 <neem693@gmail.com>
2023-04-02 22:28:59 +0900

    edit javascript for work and edit button

--- outpaint_region.py
+++ outpaint_region.py
@@ -140,20 +140,20 @@ class Script(scripts.Script):
                 HTML = `<div id='alphaWindow' style='display:none;position:absolute;min-width:500px;min-height:200px;z-index:1000;overflow:hidden !important;resize:both;border-radius:5px;border: 1px solid black;' class='gr-panel'>
                             <div id='alphaPosition' style='left:0px;top:0px;width:30px;height:30px;position:absolute;' class='gr-button gr-button-lg gr-button-secondary'>A</div>
                             <div id='alphaTitle' style='left:30px;top:0px;right:75px;height:30px;display:block;position:absolute;background:#444444;color:#ffffff;padding-left: 5px;'>AlphaCanvas</div>
-                            <div id='alphaClose' style='right:0px;top:0px;width:75px;height:30px;position:absolute' class='gr-button gr-button-lg gr-button-secondary'>Close</div>
+                            <div id='alphaClose' style='right:0px;top:0px;cursor:pointer;padding:5px;height:30px;position:absolute' class='gr-button gr-button-lg gr-button-secondary lg secondary gradio-button svelte-1ipelgc'>Close</div>
                             <input id='alphaFile' style='display:none' type='file'></input>
                             <div id='alphaTopMenu' style='left:0px;top:30px;right:0px;height:80px;display:block;position:absolute;' class='gr-panel'>
                                 <img id='alphaItem' style='left:5px;top:8px;width:64px;height:64px;display:block;position:absolute;background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAcAAAAHCAYAAADEUlfTAAAAAXNSR0IArs4c6QAAABtJREFUGFdjTKi+7cuAAzCCJBe0qm7GJj/oJAGwGxoLJP3XYQAAAABJRU5ErkJggg==)''/>
-                                <div id='alphaGrab' style='right:0px;top:15px;width:15%;height:40px;position:absolute;' class='gr-button gr-button-lg gr-button-secondary'>Grab Results</div>
-                                <div id='alphaMerge' style='right:20%;top:15px;width:15%;height:40px;position:absolute' class='gr-button gr-button-lg gr-button-secondary'>Apply Patch</div>
+                                <div id='alphaGrab' class="lg secondary gradio-button svelte-1ipelgc" style='cursor:pointer;right:0px;top:15px;padding:5px;height:40px;position:absolute;' class='gr-button gr-button-lg gr-button-secondary'>Grab Results</div>
+                                <div id='alphaMerge' class="lg secondary gradio-button svelte-1ipelgc" style='cursor:pointer;right:20%;top:15px;padding:5px;height:40px;position:absolute' class='gr-button gr-button-lg gr-button-secondary'>Apply Patch</div>
                                 <input id='alphaHue' style='left:75px;top:0px;right:70%;height:20px;display:block;position:absolute' type='range' min='-0.05', max='0.05', value='0.0' step='0.005'></input>
                                 <div id='alphaHueLabel' style='left:30%;top:0px;width:10%;height:20px;display:block;position:absolute;' class='text-gray-500'>Hue:0</div>
                                 <input id='alphaSaturation' style='left:75px;top:21px;right:70%;height:20px;display:block;position:absolute' type='range' min='-0.05', max='0.05', value='0.0' step='0.005'></input>
                                 <div id='alphaSaturationLabel' style='left:30%;top:21px;width:10%;height:20px;display:block;position:absolute;' class='text-gray-500'>S:0</div>
                                 <input id='alphaLightness' style='left:75px;top:42px;right:70%;height:20px;display:block;position:absolute' type='range' min='-0.05', max='0.05', value='0.0' step='0.005'></input>
                                 <div id='alphaLightnessLabel' style='left:30%;top:42px;width:10%;height:20px;display:block;position:absolute;' class='text-gray-500'>L:0</div>
-                                <div id='alphaUpload' style='left:42%;top:1px;width:15%;height:35px;position:absolute;' class='gr-button gr-button-lg gr-button-secondary'>Load Canvas</div>
-                                <div id='alphaDownload' style='left:42%;top:41px;width:15%;height:35px;position:absolute;' class='gr-button gr-button-lg gr-button-secondary'>Save Canvas</div>
+                                <div id='alphaUpload' class="lg secondary gradio-button svelte-1ipelgc" style='cursor:pointer;left:42%;top:1px;padding:5px;height:35px;position:absolute;' class='gr-button gr-button-lg gr-button-secondary'>Load Canvas</div>
+                                <div id='alphaDownload' class="lg secondary gradio-button svelte-1ipelgc" style='cursor:pointer;left:42%;top:41px;padding:5px;height:35px;position:absolute;' class='gr-button gr-button-lg gr-button-secondary'>Save Canvas</div>
                             </div>
                             <div id='alphaSideMenu' style='right:0px;top:110px;width:80px;bottom:10px;display:flex;position:absolute;flex-direction:column;overflow-x:hidden !important;overflow-y:auto !important;' class='gr-panel'></div>
                             <div id='alphaCanvasContainer' style='left:0px;top:110px;right:80px;bottom:0px;display:block;position:absolute;overflow:auto;background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAcAAAAHCAYAAADEUlfTAAAAAXNSR0IArs4c6QAAABtJREFUGFdjTKi+7cuAAzCCJBe0qm7GJj/oJAGwGxoLJP3XYQAAAABJRU5ErkJggg==)'>
@@ -480,10 +480,14 @@ class Script(scripts.Script):
                 // Import results
                 function getImages() {
                     let imgParent = gradioApp().getElementById('img2img_gallery');
-                    let choices_t = imgParent.getElementsByClassName('gallery-item group');
+                    let choices_t = imgParent.getElementsByClassName('thumbnail-item thumbnail-lg');
     	            if (choices_t.length==0) {
                         console.info('choices empty. Using alternative method');
                         choices_t = imgParent.getElementsByClassName('gallery-item svelte-1g9btlg');
+                    }
+                    if (choices_t.length==0) {
+                        console.info('choices empty.');
+                        alert('there is no generate image');
                     }
                     const choices = choices_t;
                     alphaSideMenu.innerHTML = '';
